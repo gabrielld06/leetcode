729034bee47fecb91bc63b2fc4521c0bc07cfbc2
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2022-08-01 15:22:25 -0300

    Time: 116 ms (47.87%), Space: 48.3 MB (93.57%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int memo[10001] = {0};
-        memo[0] = true;
         int d = 1;
-        for(int i = 0;d > 0 and i < nums.size();i++) {
-            d--;
-            memo[i] = true;
-            d = max(d, nums[i]);
+        for(int i = 0;i < nums.size();i++) {
+            if(d == 0) return false;
+            d = max(d-1, nums[i]);
         }
         
-        return memo[nums.size()-1];
+        return true;
     }
 };
