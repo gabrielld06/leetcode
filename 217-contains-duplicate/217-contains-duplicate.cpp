963e0cfd9859e97df21fc4fe219fcf85fab22507
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2022-08-03 20:27:02 -0300

    Time: 124 ms (72.28%), Space: 55.3 MB (6.18%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map<int, bool> mp;
-        
-        for(auto n : nums) {
-            if(mp[n]) return true;
-            mp[n] = true;
-        }
-        
-        return false;
+        return unordered_set<int>(nums.begin(), nums.end()).size() < nums.size();
     }
 };
