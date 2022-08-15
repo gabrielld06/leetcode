2eed6b18fe51aad20fbb678884b8d9ab288ec996
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2022-08-15 15:41:57 -0300

    Time: 0 ms (100.00%), Space: 6.2 MB (72.80%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,40 +1,49 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int i = s.length()-1, j = t.length()-1;
-        int decI = 0, decJ = 0;
-        while(i >= 0 and j >= 0) {
-            while(i >= 0 and (s[i] == '#' or decI > 0)) {
-                if(s[i] == '#') decI++;
-                else decI--;
-                i--;
+        for(int i = 0; i < s.length();i++) {
+            if(s[i] == '#') {
+                int j = i;
+                while(j >= 0 and s[j] == '#') {
+                    j--;
+                }
+                if(j >= 0) s[j] = '#';
+            }
+        }
+        
+        for(int i = 0; i < t.length();i++) {
+            if(t[i] == '#') {
+                int j = i;
+                while(j >= 0 and t[j] == '#') {
+                    j--;
+                }
+                if(j >= 0) t[j] = '#';
+            }
+        }
+        
+        int i = 0, j = 0;
+        
+        while(i < s.length() and j < t.length()) {
+            while(i < s.length() and s[i] == '#') {
+                i++;
+            }
+            while(j < t.length() and t[j] == '#') {
+                j++;
             }
             
-            while(j >= 0 and (t[j] == '#' or decJ > 0)) {
-                if(t[j] == '#') decJ++;
-                else decJ--;
-                j--;
-            }
-
-            if(j < 0 or i < 0) break;
-            
+            if(i == s.length() or j == t.length()) break;
             if(s[i] != t[j]) return false;
             
-            i--;j--;
-        }        
-        
-        while(i >= 0 and (s[i] == '#' or decI > 0)) {
-            if(s[i] == '#') decI++;
-            else decI--;
-            i--;
-        }
-
-        while(j >= 0 and (t[j] == '#' or decJ > 0)) {
-            if(t[j] == '#') decJ++;
-            else decJ--;
-            j--;
+            i++;j++;
         }
         
-        return (i == j);
+        while(i < s.length() and s[i] == '#') {
+            i++;
+        }
+        while(j < t.length() and t[j] == '#') {
+            j++;
+        }
+        
+        return i == s.length() and j == t.length();
     }
 };
