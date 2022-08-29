449fcf74f778979f095b5b392dc8d17267e1f285
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2022-08-29 11:13:43 -0300

    Time: 5 ms (90.69%), Space: 7.4 MB (45.88%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        if(s1.length() > s2.length()) return false;
         vector<int> mp1(26, 0);
-        for(auto c : s1) {
-            mp1[c - 'a']++;
-        }
         vector<int> mp2(26, 0);
-        for(auto c : s2) {
-            mp2[c - 'a']++;
+        for(int i = 0;i < s1.length();i++) {
+            mp1[s1[i] - 'a']++;
+            mp2[s2[i] - 'a']++;
         }
         
-        bool f = false;
+        int count = 0;
+        for(int i = 0;i < 26;i++) {
+            if(mp1[i] == mp2[i]) count++;
+        }
         
-        for(int i = 0;i + s1.length() <= s2.length();i++) {
-            if(mp1[s2[i] - 'a'] > 0) {
-                vector<int> mp3(26, 0);
-                f = false;
-                for(int j = 0;j < s1.length();j++) {
-                    mp3[s2[i + j] - 'a']++;
-                    if(mp1[s2[i + j]  - 'a'] < mp3[s2[i + j] - 'a']) {
-                        f = true;
-                        break;
-                    }
-                }
-                if(not f) return true;
-            }
+        for(int i = 0;i + s1.length() < s2.length();i++) {
+            if(count == 26) return true;
             
-            mp2[s2[i]  - 'a']--;
-            if(mp2[s2[i]  - 'a'] < mp1[s2[i]  - 'a']) return false;
+            int left = s2[i] - 'a', right = s2[i + s1.length()] - 'a';
+            
+            mp2[right]++;
+            if(mp2[right] == mp1[right]) count++;
+            else if(mp2[right] == mp1[right] + 1) count--;
+            
+            mp2[left]--;
+            if(mp2[left] == mp1[left]) count++;
+            else if(mp2[left] == mp1[left] - 1) count--;
         }
-        return false;
+        return count == 26;
     }
 };
