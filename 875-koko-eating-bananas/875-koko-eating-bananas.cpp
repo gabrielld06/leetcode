e5e4525bf1b8b46a61b61f92045b8156a40fe6ae
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2022-09-05 10:27:04 -0300

    Time: 69 ms (61.69%), Space: 18.8 MB (73.96%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int comecome(vector<int>& piles, int ans) {
+        int r = 0;
+        for(auto e : piles) {
+            r += ((e - 1) / ans) + 1;
+        }
+        
+        return r;
+    }
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        long max_e = piles[0];
+        
+        for(int i = 1;i < piles.size();i++) {
+            if(piles[i] > max_e) max_e = piles[i];
+        }
+        
+        if(piles.size() == h) return max_e;
+        
+        long mid, r, b = 1, e = max_e;
+        int ans = max_e;
+        while(b < e) {
+            mid = b + (e - b) / 2;
+            
+            r = comecome(piles, mid);
+            
+            if(r == h and mid < ans) {
+                ans = mid;
+                e = mid;
+            }
+            else if(r == h and mid >= ans) return ans;
+            else if(r < h) e = mid;
+            else if(r > h) b = mid + 1;
+        }
+        return e;
+    }
+};
