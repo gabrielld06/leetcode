a030a416bcc06524db722205d2d44a4a1b8b3bbc
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2023-10-06 08:47:05 -0300

    Time: 11 ms (51.95%), Space: 16.2 MB (19.58%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,11 +2,41 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int min_appearence = nums.size()/3 + 1;
-        vector<int> ans;
-        map<int, int> appearence;
+        int count1 = 0, count2 = 0, candidate1 = 0, candidate2 = 0;
+
         for(int i = 0;i < nums.size();i++) {
-            appearence[nums[i]]++;
-            if(appearence[nums[i]] == min_appearence) ans.push_back(nums[i]); 
+             if(count1 == 0 && nums[i] != candidate2) {
+                candidate1 = nums[i];
+                count1++;
+            } else if(count2 == 0 && nums[i] != candidate1) {
+                candidate2 = nums[i];
+                count2++;
+            } else if(nums[i] == candidate1) {
+                count1++;
+            } else if(nums[i] == candidate2) {
+                count2++;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0, count2 = 0;
+        for(int i = 0;i < nums.size();i++) {
+            if(nums[i] == candidate1) {
+                count1++;
+            } else if(nums[i] == candidate2) {
+                count2++;
+            }
+        }
+
+        vector<int> ans;
+
+        if(count1 >= min_appearence) {
+            ans.push_back(candidate1);
+        }
+        if(count2 >= min_appearence) {
+            ans.push_back(candidate2);
         }
 
         return ans;
