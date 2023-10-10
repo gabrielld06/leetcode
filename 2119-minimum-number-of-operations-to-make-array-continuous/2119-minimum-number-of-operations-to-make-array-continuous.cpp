68abbdd3cfc669bbc550365c3eabb4a839628735
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2023-10-10 09:16:05 -0300

    Time: 407 ms (11.51%), Space: 109.9 MB (10.67%) - LeetHub

--- 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2119-minimum-number-of-operations-to-make-array-continuous/2119-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int bin_search(vector<int>& nums, int i, int j, int target) {
-        if(i > j) return i;
-
-        int mid = i + (j - i) / 2;
-
-        if(nums[mid] == target) {
-            return mid + 1;
-        }
-
-        if(nums[mid] < target) {
-            return bin_search(nums, mid+1, j, target);
-        } else {
-            return bin_search(nums, i, mid-1, target);
-        }
-    }
-
     int minOperations(vector<int>& nums) {
         set<int> seen;
         vector<int> vet;
+
         for(auto e : nums) {
             if(seen.find(e) != seen.end()) continue;
             seen.insert(e);
+        }
+
+        for(auto e : seen) {
             vet.push_back(e);
         }
-        sort(vet.begin(), vet.end());
+
         int min_operations = nums.size();
         for(int i = 0; i < vet.size();i++) {
             int target =  vet[i] + nums.size() - 1;
