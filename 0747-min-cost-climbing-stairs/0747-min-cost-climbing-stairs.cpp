496b7bc7893fce1fd717d9fb542f6fa4f1afef7f
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2023-10-13 09:14:37 -0300

    Time: 4 ms (61.59%), Space: 13.9 MB (52.88%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int memo[1001];
+        int memo[1001], n = cost.size();
 
         memo[0] = cost[0];
         memo[1] = cost[1];
 
-        for(int i = 2;i < cost.size();i++) {
+        for(int i = 2;i < n;i++) {
             memo[i] = (memo[i - 1] < memo[i - 2] ? memo[i - 1] : memo[i - 2]) + cost[i];
         }
 
-        return (memo[cost.size() - 1] < memo[cost.size() - 2] ? memo[cost.size() - 1] : memo[cost.size() - 2]);
+        return (memo[n - 1] < memo[n - 2] ? memo[n - 1] : memo[n - 2]);
     }
 };
