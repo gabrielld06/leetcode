f0e80c1c9c4ba2b814b61280c03b04bbe7405b6b
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2023-10-25 08:50:35 -0300

    Time: 0 ms (100%), Space: 6.24 MB (53.37%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
+    int fastPow(int n, int exp) {
+        if(exp == 0) return 1;
+
+        if(exp & 1) {
+            int p = fastPow(n, (exp-1) / 2);
+
+            return p * p * n;
+        } else {
+            int p = fastPow(n, exp / 2);
+
+            return p * p;
+        }
+    }
+
     int binSearch(int n, int k, int x, int b, int e) {
         if(n == 1) return x;
 
         int m = (b + e - 1) / 2;
         if(x == 0) {
             // 0 1
-            // if(b == k) return 0; 
-            // if(e == k) return 1; 
             if(k > m) {
                 return binSearch(n-1, k, 1, m+1, e);
             } else {
@@ -15,8 +27,6 @@ public:
             }
         } else {
             // 1 0
-            // if(b == k) return 1; 
-            // if(e == k) return 0;
             if(k > m) {
                 return binSearch(n-1, k, 0, m+1, e);
             } else {
@@ -26,7 +36,7 @@ public:
     }
 
     int kthGrammar(int n, int k) {
-        int e = pow(2, n-1);
+        int e = fastPow(2, n-1);
 
         return binSearch(n, k, 0, 1, e);
     }
