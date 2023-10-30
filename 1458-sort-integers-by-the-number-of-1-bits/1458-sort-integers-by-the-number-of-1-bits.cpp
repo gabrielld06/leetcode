8ebc7727b10b50cf37298a8871cafbfff2c85226
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2023-10-30 08:29:42 -0300

    Time: 11 ms (32.06%), Space: 10.7 MB (45.81%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,3 +1,5 @@
+int memo[10001];
+
 class Solution {
 public:
     static bool countBits(int x, int y) {
@@ -5,16 +7,30 @@ public:
         int num1 = x, num2 = y;
 
         while(x > 0) {
+            if(memo[x] != -1) {
+                count_x += memo[x];
+                break;
+            }
+
             count_x += x & 1;
 
             x = x >> 1;
         }
+
+        memo[x] = count_x;
         
         while(y > 0) {
+            if(memo[y] != -1) {
+                count_y += memo[y];
+                break;
+            }
+
             count_y += y & 1;
 
            y = y >> 1;
         }
+
+        memo[y] = count_y;
 
         if(count_x == count_y) {
             return num1 < num2;
@@ -24,6 +40,8 @@ public:
     }
 
     vector<int> sortByBits(vector<int>& arr) {
+        memset(memo, -1, sizeof(memo));
+
         std::sort(arr.begin(), arr.end(), countBits);
 
         return arr;
