39a8fff8e944ef0e7c95b28aa298240de184a9ed
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2023-10-30 08:40:34 -0300

    Time: 8 ms (53.22%), Space: 10.4 MB (81%) - LeetHub

--- 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,44 +1,28 @@
-int memo[10001] = {0};
+bool countBits(int const &num1, int const &num2) {
+    int count_x = 0, count_y = 0;
+    int x = num1, y = num2;
+
+    while(x > 0) {
+        count_x += x & 1;
+
+        x >>= 1;
+    }
+    
+    while(y > 0) {
+        count_y += y & 1;
+
+        y >>= 1;
+    }
+
+    if(count_x == count_y) {
+        return num1 < num2;
+    }
+
+    return count_x < count_y;
+}
 
 class Solution {
 public:
-    static bool countBits(int x, int y) {
-        int count_x = 0, count_y = 0;
-        int num1 = x, num2 = y;
-
-        while(x > 0) {
-            if(memo[x] != 0) {
-                count_x += memo[x];
-                break;
-            }
-
-            count_x += x & 1;
-
-            x = x >> 1;
-        }
-
-        memo[x] = count_x;
-        
-        while(y > 0) {
-            if(memo[y] != 0) {
-                count_y += memo[y];
-                break;
-            }
-
-            count_y += y & 1;
-
-           y = y >> 1;
-        }
-
-        memo[y] = count_y;
-
-        if(count_x == count_y) {
-            return num1 < num2;
-        }
-
-        return count_x < count_y;
-    }
-
     vector<int> sortByBits(vector<int>& arr) {
         std::sort(arr.begin(), arr.end(), countBits);
 
