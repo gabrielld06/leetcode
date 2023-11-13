0e51e9bab69b8816ff0681cde6d395b77683260f
Gabriel Dias <64218838+gabrielld06@users.noreply.github.com>
2023-11-13 08:54:26 -0300

    Time: 4 ms (26.99%), Space: 6.4 MB (12.86%) - LeetHub

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/3056-determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/3056-determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -1,28 +1,5 @@
 class Solution {
 public:
-    bool travel(int x, int y, int fx, int fy, int t) {
-        if(x == fx && y == fy) {
-            return true;
-        }
-
-        if(t == 0) {
-            return false;
-        }
-
-        if(fx > x) {
-            x++;
-        } else if(fx < x) {
-            x--;
-        }
-    
-        if(fy > y) {
-            y++;
-        } else if(fy < y) {
-            y--;
-        }
-
-        return travel(x, y, fx, fy, t-1);
-    }
     bool isReachableAtTime(int x, int y, int fx, int fy, int t) {
         if(x == fx && y == fy && t == 1) return false;
         if(x == fx && y == fy) return true;
@@ -42,7 +19,7 @@ public:
         }
 
         int l = abs(x - fx) + abs(y - fy);
-        
+
         return d + l <= t;
     }
 };
